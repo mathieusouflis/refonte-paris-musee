78eaf4528ba58ae48b623fe328e46de945885f2d
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-10-14 14:27:42 +0200

    refactor: Simplify Home page component structure

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,3 @@
-import { H1 } from "@/components/base/typography/h1";
-
 export default function Home() {
-  return (
-    <div>
-      <main>
-        <H1>Hi</H1>
-      </main>
-    </div>
-  );
+  return <div></div>;
 }
